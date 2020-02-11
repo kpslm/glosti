50dfffcd115b684d18687be83ca7e8d402f0a17f
kpslm <46449204+kpslm@users.noreply.github.com>
2020-02-11 17:01:56 +0100

    Add files via upload

---
+++ site/js/ordonnance.js
@@ -0,0 +1,8 @@
+
+document.getElementById("resultNom").innerHTML=localStorage.getItem("nom");
+document.getElementById("resultPrenom").innerHTML=localStorage.getItem("prenom");
+document.getElementById("resultNUMERO D’IDENTIFICATION").innerHTML=localStorage.getItem("NUMERO D’IDENTIFICATION");
+document.getElementById("resultMAIL").innerHTML=localStorage.getItem("MAIL");
+document.getElementById("resultNUMERO DE TELLEPHONE").innerHTML=localStorage.getItem("NUMERO DE TELLEPHONE");
+document.getElementById("resultCONTENU DE L'ORDONNANCE").innerHTML=localStorage.getItem("CONTENU DE L'ORDONNANCE");
+document.getElementById("resultCONTENU creer ordonnance").innerHTML=localStorage.getItem("creer ordonnance");
